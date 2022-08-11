82675f24651671bd944fdcde2776dab62b0bb174
niewiemczego <tymek.sikora@gmail.com>
2022-08-12 00:58:45 +0200

    bubble + quick sort

--- main.py
+++ main.py
@@ -33,8 +33,57 @@ class SortingVisualizer:
                 control.disabled = True
         self.page.update()
 
+    def partition(self, low, high):
+        pivot = self.to_sort_lines.content.controls[high]
+        i = low - 1
+
+        for j in range(low, high):
+            if self.to_sort_lines.content.controls[j].height <= pivot.height:
+                i += 1
+                (self.to_sort_lines.content.controls[i].bgcolor, self.to_sort_lines.content.controls[j].bgcolor) = (colors.RED, colors.RED)
+                self.to_sort_lines.content.update()
+                time.sleep(0.01)
+                (self.to_sort_lines.content.controls[i].height, self.to_sort_lines.content.controls[j].height) = (self.to_sort_lines.content.controls[j].height, self.to_sort_lines.content.controls[i].height)
+                self.to_sort_lines.content.update()
+                (self.to_sort_lines.content.controls[i].bgcolor, self.to_sort_lines.content.controls[j].bgcolor) = (colors.GREEN, colors.GREEN)
+                self.to_sort_lines.content.update()
+                time.sleep(0.01)
+        (self.to_sort_lines.content.controls[i + 1].height, self.to_sort_lines.content.controls[high].height) = (self.to_sort_lines.content.controls[high].height, self.to_sort_lines.content.controls[i + 1].height)
+        self.to_sort_lines.content.update()
+        (self.to_sort_lines.content.controls[i + 1].bgcolor, self.to_sort_lines.content.controls[high].bgcolor) = (colors.GREEN, colors.GREEN)
+        self.to_sort_lines.content.update()
+        return i + 1
+
+    def quick_sort_vis(self, low = 0, high = None):
+        if low < high:
+            if not high:
+                high = len(self.to_sort_lines.content.controls)-1
+            pi = self.partition(low, high)
+            self.quick_sort_vis(low, pi - 1)
+            self.quick_sort_vis(pi + 1, high)
+
+    def quick_sort(self, e) -> None:
+        self.quick_sort_vis(0, len(self.to_sort_lines.content.controls)-1)
+    
     def bubble_sort(self, e) -> None:
-        pass
+        self.able_disable_during_sorting()
+        for i in range(len(self.to_sort_lines.content.controls)):
+            for j in range(len(self.to_sort_lines.content.controls)-1-i):
+                if self.to_sort_lines.content.controls[j].height > self.to_sort_lines.content.controls[j+1].height:
+                    self.to_sort_lines.content.controls[j].bgcolor = colors.RED
+                    self.to_sort_lines.content.controls[j+1].bgcolor = colors.RED
+                    self.to_sort_lines.content.update()
+                    time.sleep(0.01)
+                    self.to_sort_lines.content.controls[j].height, self.to_sort_lines.content.controls[j+1].height = self.to_sort_lines.content.controls[j+1].height, self.to_sort_lines.content.controls[j].height
+                    self.to_sort_lines.content.update()
+                    self.to_sort_lines.content.controls[j].bgcolor = colors.GREEN
+                    self.to_sort_lines.content.controls[j+1].bgcolor = colors.GREEN
+                    self.to_sort_lines.content.update()
+                    time.sleep(0.01)
+                self.to_sort_lines.content.controls[j].bgcolor = colors.LIGHT_BLUE
+                self.to_sort_lines.content.controls[j+1].bgcolor = colors.LIGHT_BLUE
+                self.to_sort_lines.content.update()
+        self.able_disable_during_sorting()
 
     def generate_new_array(self, e) -> None:
         self.to_sort_lines.content.controls.clear()
@@ -54,6 +103,7 @@ class SortingVisualizer:
         self.slider = Slider(value=10, min=10, max=84, divisions=74, label="{value}", on_change=self.generate_new_array)
         self.gen_new_arr_btn = TextButton(text="Generate New Array", on_click=self.generate_new_array)
         self.bubble_sort_btn = TextButton(text="Bubble Sort", on_click=self.bubble_sort)
+        self.merge_sort_btn = TextButton(text="Quick Sort", on_click=self.quick_sort)
         self.top_menu = Container(
             content=Row(
                 controls=[
@@ -61,6 +111,7 @@ class SortingVisualizer:
                     Text(value="Change Array Size & Sorting Speed"),
                     self.slider,
                     self.bubble_sort_btn,
+                    self.merge_sort_btn
                 ]
             ),
             margin=margin.only(top=40)
